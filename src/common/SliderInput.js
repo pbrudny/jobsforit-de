558c7cb1b891c964cf78fbd6aa026f40f4e72754
Piotr Brudny <pbrudny@gmail.com>
2023-09-10 14:42:51 +0200

    Refactor SliderInput to Functional Component
    - Converted SliderInput from class component to functional component.
    
    - Utilized hooks for state management.
    
    - Moved formatNumber function into the component body.
    
    - Destructured props for cleaner and more concise code.
    
    - Improved accessibility by adding alt attributes to img tags.
    
    For more details, check: https://chat.openai.com/share/722a4dd0-6544-41db-aeb4-38b49547f572

--- src/common/SliderInput.js
+++ src/common/SliderInput.js
@@ -1,71 +1,62 @@
 import React from 'react';
-
 import style from './SliderInput.module.scss';
 import tooltipBottomGray from 'assets/img/icons-new-design/tooltip-bottom--gray.svg';
 import Slider from "@material-ui/core/Slider";
 
-class SliderInput extends React.Component {
-
-  formatNumber(num, prefix = '') {
+const SliderInput = ({ min, max, value, step, prefix, onChange }) => {
+  const formatNumber = (num, prefix = '') => {
     return prefix + num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   }
 
-  render() {
-    const {min, max, value, step, prefix} = this.props;
-    const [valueBottom, valueTop] = value;
-    const percentageValueBottom = (valueBottom / max) * 100 + '%';
-    const percentageValueTop = (valueTop / max) * 100 + '%';
+  const [valueBottom, valueTop] = value;
+  const percentageValueBottom = (valueBottom / max) * 100 + '%';
+  const percentageValueTop = (valueTop / max) * 100 + '%';
 
-    const minFormatted = this.formatNumber(min, prefix);
-    const maxFormatted = this.formatNumber(max, prefix);
-    const valueBottomFormatted = this.formatNumber(valueBottom, prefix);
-    const valueTopFormatted = this.formatNumber(valueTop, prefix);
+  const minFormatted = formatNumber(min, prefix);
+  const maxFormatted = formatNumber(max, prefix);
+  const valueBottomFormatted = formatNumber(valueBottom, prefix);
+  const valueTopFormatted = formatNumber(valueTop, prefix);
 
-    const tooltipBottomPositionValue = Number(((valueBottom - min) * 100) / (max - min));
-    const tooltipTopPositionValue = Number(((valueTop - min) * 100) / (max - min));
+  const tooltipBottomPositionValue = Number(((valueBottom - min) * 100) / (max - min));
+  const tooltipTopPositionValue = Number(((valueTop - min) * 100) / (max - min));
 
-    return (
-      <div className={style.SliderInput}>
-        <div
-          className={style.SliderInput_tooltip}
-          style={{left: `calc(${tooltipBottomPositionValue}%`}}
-        >
-          <img
-            src={tooltipBottomGray}
-          />
-          {valueBottomFormatted}
-        </div>
-
-        <div className={style.SliderInput_tooltip}
-             style={{left: `calc(${tooltipTopPositionValue}%)`}}>
-          <img
-            src={tooltipBottomGray}
-          />
-          {valueTopFormatted}
-        </div>
-
-        <div className={style.SliderInput_input}>
-          <Slider
-            min={0}
-            step={2000}
-            max={200000}
-            value={value}
-            onChange={this.props.onChange}
-            aria-labelledby="range-slider"
-          />
-        </div>
-
-        <div className={style.SliderInput_bottom}>
-          <span className={style.SliderInput_label}>
-            {minFormatted}
-          </span>
-          <span className={style.SliderInput_label}>
-            {maxFormatted}
-          </span>
-        </div>
+  return (
+    <div className={style.SliderInput}>
+      <div
+        className={style.SliderInput_tooltip}
+        style={{ left: `calc(${tooltipBottomPositionValue}%)` }}
+      >
+        <img src={tooltipBottomGray} alt="Tooltip Bottom" />
+        {valueBottomFormatted}
       </div>
-    )
-  }
+
+      <div className={style.SliderInput_tooltip}
+           style={{ left: `calc(${tooltipTopPositionValue}%)` }}>
+        <img src={tooltipBottomGray} alt="Tooltip Top" />
+        {valueTopFormatted}
+      </div>
+
+      <div className={style.SliderInput_input}>
+        <Slider
+          min={0}
+          step={2000}
+          max={200000}
+          value={value}
+          onChange={onChange}
+          aria-labelledby="range-slider"
+        />
+      </div>
+
+      <div className={style.SliderInput_bottom}>
+        <span className={style.SliderInput_label}>
+          {minFormatted}
+        </span>
+        <span className={style.SliderInput_label}>
+          {maxFormatted}
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default SliderInput;
