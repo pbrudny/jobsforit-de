43567a7a474928efd26c3bc9647087a7bba0e3f2
Piotr Brudny <pbrudny@gmail.com>
2023-09-10 13:59:11 +0200

    Refactor Hero component to functional style
    - Converted from class component to functional component.
    - Utilized useContext hook for accessing ThemeContext.
    - Destructured props for clearer code readability.
    - Replaced join(' ') with template literals for better string concatenation.
    
    ChatGPT shared link: https://chat.openai.com/share/722a4dd0-6544-41db-aeb4-38b49547f572

--- src/common/Hero.js
+++ src/common/Hero.js
@@ -1,48 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Container from "./Container/Container";
-import {ThemeContext} from "../themeContext";
+import { ThemeContext } from "../themeContext";
 import Heading from "./Heading";
-import {toJS} from 'mobx';
 import style from './Hero.module.scss';
 
-class Hero extends React.Component {
-  render() {
-    const themeContext = this.context;
+const Hero = ({ heading, description, heroImage }) => {
+  const themeContext = useContext(ThemeContext);
 
-    const classes = [style.Hero];
+  const classes = [style.Hero];
 
-    if (themeContext.theme === 'dark') {
-      classes.push(style.Hero_dark);
-    } else {
-      classes.push(style.Hero_light);
-    }
-
-    return (
-      <div className={classes.join(' ')}>
-        <Container>
-          <div className={style.Hero__cols}>
-            <div className={[style.Hero__col, style.Hero__col_content].join(' ')}>
-              <Heading variant='h2' theme={themeContext.theme} className={style.Hero__heading}>
-                {this.props.heading}
-              </Heading>
-              <p className={style.Hero__description}>
-                {this.props.description}
-              </p>
-            </div>
-            <div className={[style.Hero__col, style.Hero__col_img].join(' ')}>
-              <img
-                className={style.Hero__img}
-                 src={this.props.heroImage.fields.url || this.props.heroImage.fields.image.fields.file.url}
-                 alt={this.props.heroImage.fields.name}
-              />
-            </div>
-          </div>
-        </Container>
-      </div>
-    )
+  if (themeContext.theme === 'dark') {
+    classes.push(style.Hero_dark);
+  } else {
+    classes.push(style.Hero_light);
   }
-}
 
-Hero.contextType = ThemeContext;
+  const imageUrl = heroImage.fields.url || heroImage.fields.image.fields.file.url;
+  const imageAlt = heroImage.fields.name;
+
+  return (
+    <div className={classes.join(' ')}>
+      <Container>
+        <div className={style.Hero__cols}>
+          <div className={`${style.Hero__col} ${style.Hero__col_content}`}>
+            <Heading variant='h2' theme={themeContext.theme} className={style.Hero__heading}>
+              {heading}
+            </Heading>
+            <p className={style.Hero__description}>
+              {description}
+            </p>
+          </div>
+          <div className={`${style.Hero__col} ${style.Hero__col_img}`}>
+            <img
+              className={style.Hero__img}
+              src={imageUrl}
+              alt={imageAlt}
+            />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
 export default Hero;
