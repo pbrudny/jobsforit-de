d9535560f57d0cc1338c328b8320ab7863b6b531
Piotr Brudny <pbrudny@gmail.com>
2023-09-10 13:58:00 +0200

    Refactor Loader component to functional style
    - Transitioned from class component to functional component.
    - Simplified the return statement for better readability.

--- src/common/Loader.js
+++ src/common/Loader.js
@@ -1,15 +1,12 @@
 import React from 'react';
-
 import style from './Loader.module.scss';
 
-class Loader extends React.Component {
-    render() {
-        return(
-            <div className={style.Loader}>
-                <span></span>
-            </div>
-        )
-    }
+const Loader = () => {
+    return (
+      <div className={style.Loader}>
+          <span></span>
+      </div>
+    );
 }
 
 export default Loader;
